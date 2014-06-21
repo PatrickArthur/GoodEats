75844f96b6597e5262a3cf757ac513e2ddb52d09
Patrick <pa@arthurfamily.ws>
2014-06-21 17:01:24 -0400

    added review in restaurant show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
 
   def index
     @restaurants = Restaurant.all
-    @reviews = review.all
+    @reviews = Review.all
   end
 
 
@@ -23,6 +23,6 @@ class ReviewsController < ApplicationController
 
   private
     def review_params
-      params.require(:review).permit(:review, :actor, :description)
+      params.require(:review).permit(:rating, :body)
     end
 end
