85fb705f0873af6ea84549b9f2050a437d92cbb3
Patrick <pa@arthurfamily.ws>
2014-06-21 16:46:53 -0400

    add text area dropdowns and review form'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
 resources :restaurants, only: [:index, :show, :new, :create] do
-    resources :reviews, only: [:create , :destroy]
+    resources :reviews, only: [:create]
   end
 end
